af8b2b43a5711ecd0e0b9f5fc5e4a1422fef9ece
Torsten <info@by-torsten.com>
2021-12-22 20:30:27 +0100

    feat: introduce defaultConfig

--- src/utils/validatePanel.ts
+++ src/utils/validatePanel.ts
@@ -6,6 +6,29 @@ import { validatePanelAst } from './validatePanelAst'
 
 export const validatePanel = (info: ModuleInfo, name: string) =>
   validatePanelAst(name, info.ast as Node, {
+    defaultConfig: declaration => {
+      if (
+        declaration.type === 'FunctionDeclaration' ||
+        !('name' in declaration.id)
+      ) {
+        return
+      }
+
+      if (!declaration.init) {
+        return {
+          error: chalk`the export {bold ${declaration.id.name}} of {bold ${name}} needs a value.`,
+        }
+      }
+
+      if (
+        declaration.init.type !== 'ObjectExpression' &&
+        declaration.init.type !== 'ArrowFunctionExpression'
+      ) {
+        return {
+          error: chalk`the export {bold ${declaration.id.name}} of {bold ${name}} has an invalid type. Only object expressions and arrow functions are allowed.`,
+        }
+      }
+    },
     description: declaration => {
       if (
         declaration.type === 'FunctionDeclaration' ||
